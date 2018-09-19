aea5aa821d05f9dc4b64e3c97b9e4dc7a9de3bf0
Pierce Corcoran <pierce@plasticcow.com>
2018-09-19 12:21:49 -0700

    Change argument order and fix --force default

--- src/main/kotlin/com/goodnighttales/xcassets/XCAssetsMain.kt
+++ src/main/kotlin/com/goodnighttales/xcassets/XCAssetsMain.kt
@@ -12,14 +12,14 @@ import com.goodnighttales.xcassets.optimizer.XCAssetsOptimizer
 import java.io.File
 
 class XCAssetsMain : CliktCommand(name = "xcassets") {
-    val input: List<File> by argument("input", "The input images and directories")
-            .file(exists = true, fileOkay = true, folderOkay = true, readable = true)
-            .multiple()
     val output: File by argument("output", "The output .xcassets directory")
             .file(exists = true, fileOkay = false, folderOkay = true, writable = true, readable = true)
             .validate { require(it.extension == "xcassets") { "invalid extension ${it.extension}, expecting xcassets"} }
+    val input: List<File> by argument("input", "The input images and directories")
+            .file(exists = true, fileOkay = true, folderOkay = true, readable = true)
+            .multiple()
     val crush: Boolean by option("-c", "--crush", help = "Enable PNGCrush").flag("--no-crush", default = true)
-    val force: Boolean by option("-f", "--force", help = "Don't check file modification dates").flag("--no-force", default = true)
+    val force: Boolean by option("-f", "--force", help = "Don't check file modification dates").flag("--no-force", default = false)
 
     override fun run() {
         val optimizer = XCAssetsOptimizer(input, output, crush, force)
